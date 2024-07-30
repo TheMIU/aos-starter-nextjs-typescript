f62abd6b43836a3d851245b4730d4aa7e206fa85
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-07-30 16:28:43 +0530

    text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "@/app/page.module.css";
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.main}>
+      <div className={styles.headingContainer}>
+        <h1>AOS + Next.Js + TypeScript</h1>
+        <br />
+        <p>Scroll Down </p>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <div className={styles.cardContainer}>
+        <div className={styles.card}>
+          <h2 className={styles.title}>Example 1</h2>
+          <p className={styles.para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            quis consequatur, deleniti quasi distinctio dolorum, totam non nobis
+            expedita obcaecati quisquam ad odio. Soluta laudantium eligendi
+            reprehenderit amet est ut!
           </p>
-        </a>
+        </div>
+
+        <div className={styles.card}>
+          <h2 className={styles.title}>Example 2</h2>
+          <p className={styles.para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            quis consequatur, deleniti quasi distinctio dolorum, totam non nobis
+            expedita obcaecati quisquam ad odio. Soluta laudantium eligendi
+            reprehenderit amet est ut!
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <h2 className={styles.title}>Example 3</h2>
+          <p className={styles.para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            quis consequatur, deleniti quasi distinctio dolorum, totam non nobis
+            expedita obcaecati quisquam ad odio. Soluta laudantium eligendi
+            reprehenderit amet est ut!
+          </p>
+        </div>
+
       </div>
-    </main>
+    </div>
   );
 }
